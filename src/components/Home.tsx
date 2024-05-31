90b576025d519dc3b9c2b4d4fd7708e1e839bdcf
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-05-31 20:59:33 +0200

    Fetch the data from API

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,28 @@
+import "./Home.css"
+import { useState, useEffect } from 'react'
+
 export default function Home(){
-  return <div>
-    Home
-  </div>
+  const [data, setData] = useState<any>()
+  console.log(new Date().toLocaleDateString('en-CA').split('T')[0])
+
+  const fetchData = () => {
+    const apiKey: String = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFXd";
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`).then(response => response.json()).then(data => setData(data))
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, [])
+console.log(data)
+
+  return data ? <div>
+      <img className="universe-img" src={data.url} alt="..."/>
+      <div>
+        <p>{data.date}</p>
+        <h4>{data.title}</h4>
+        <p>Copyright: {data.copyright}</p>
+        <p>{data.explanation}</p>
+      </div>
+    </div> :
+    <h2>Loading...</h2>
 }
