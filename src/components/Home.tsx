e5449f1a057b4d24907dec2d588bb68483f9afc0
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-06-01 11:37:08 +0200

    Add date picker feature

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,25 +2,39 @@ import "./Home.css"
 import { useState, useEffect } from 'react'
 
 export default function Home(){
-  const [data, setData] = useState<any>()
+  let today = new Date().toLocaleDateString('en-CA')
+  console.log(today)
+  const [data, setData] = useState<any>();
+  const [date, setDate] = useState<string>(today);
 
   const fetchData = () => {
     const apiKey: String = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFXd";
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`).then(response => response.json()).then(data => setData(data))
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`).then(response => response.json()).then(data => setData(data))
   }
 
   useEffect(()=> {
     fetchData()
-  }, [])
-console.log(data)
+  }, [date])
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(event.target.value)
+  }
+
+  console.log(data)
 
   return data ? <div className="container">
       <h1>Astronomy Picture of the Day</h1>
       <h2>{data.title}</h2>
       <img className="universe-img" src={data.url} alt="..."/>
       <div>
+        <input 
+          type="date" 
+          onChange={handleChange}
+          value={date}
+          max={today}
+          min="1995-06-16"
+        />
         <p>{data.date}</p>
-        <p>Copyright: {data.copyright}</p>
         <p>{data.explanation}</p>
       </div>
     </div> :
