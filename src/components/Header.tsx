3f40c0c6aa95cd3528abcb49de95d0661d1f6ce9
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-06-01 17:03:15 +0200

    Add links in header element and style it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import "./Header.css"
-import { NavLink } from "react-router-dom";
+import { NavLink , Link } from "react-router-dom";
 
 export default function Header() {
   return <div className="header-container">
-    <h1 className="page-title">Astronomy Picture of the Day</h1>
-    <h1 className="small-screen-title">APOD</h1>
+    <Link to="/">
+      <h1 className="page-title">Astronomy Picture of the Day</h1>
+      <h1 className="small-screen-title">APOD</h1>
+    </Link>
     <NavLink className="button" to="/gallery">
       <p>See gallery</p>
     </NavLink>
