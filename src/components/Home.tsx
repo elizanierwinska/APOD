a10fe2d6e459bbe98581e65a0f2b2cb332336c52
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-06-02 14:54:13 +0200

    Install axios to fetch the data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,49 @@
 import "./Home.css"
 import { useState, useEffect } from 'react'
+import { FetchedData } from "../types";
+import axios from "axios";
 
 export default function Home(){
   let today = new Date().toLocaleDateString('en-CA')
-  console.log(today)
-  const [data, setData] = useState<any>();
+  const [data, setData] = useState<FetchedData | any>();
   const [date, setDate] = useState<string>(today);
-
-  const fetchData = () => {
-    const apiKey: String = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFXd";
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}&count`).then(response => response.json()).then(data => setData(data))
-  }
+  const [error, setError] = useState(null)
 
   useEffect(()=> {
-    fetchData()
+    const apiKey: String = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFX";
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`).then(data => setData(data)).catch(error => setError(error))
   }, [date])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDate(event.target.value)
   }
+// console.log(error, data)
+if(error){
+  console.log(error)
+}
 
-  console.log(data)
+  // if(data?.error || data?.code) {
+  //   return <div className="error">
+  //       <h1>An Error occured</h1>
+  //       <p>
+  //         <b>Error message:</b> {data?.error?.message || data.msg}
+  //       </p>
+  //       <p className="instructions">Try to visit the page later or try to pick the date from the date picker below.</p>
+  //       <input 
+  //           className="date-picker"
+  //           type="date" 
+  //           onChange={handleChange}
+  //           value={date}
+  //           max={today}
+  //           min="1995-06-16"
+  //         />
+  //     </div>
+  // }
 
   return data ? <div className="container">
       {data.media_type === "image" ?
       <img className="universe-img" src={data.url} alt="..."/> : data.media_type === "video" ? <iframe className="youtube-video" src={data.url} title="Apod" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> :
-     <p>Sorry, the media you are searching for is not found. Please pick another date</p>}
+     <p className="no-img">Sorry, the media you are searching for is not found. Please pick another date to see the photo.</p>}
       <div className="description">
         <h2 className="img-title">{data.title}</h2>
         <div className="date-container">
@@ -35,7 +53,6 @@ export default function Home(){
             type="date" 
             onChange={handleChange}
             value={date}
-            max={today}
             min="1995-06-16"
           />
         </div>
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
-export{}
+export interface FetchedData {
+  date: string;
+  explanation: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string
+}
