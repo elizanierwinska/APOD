6ca0e7a1d7309f5ccc02b32e86f50ccbd3925230
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-06-05 20:41:14 +0200

    Create picture component to display picture

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,15 +3,13 @@ import { useEffect } from 'react';
 import { FetchedData, Props} from "../types";
 import Error from "./Error";
 import fetchData from "../fetchData";
+import Picture from "./Picture";
 
 export default function Home({date, data, setData, error, setError}: Props) {
 
   useEffect(()=> {
     fetchData(date,setData,setError)
   }, [date])
-
-
-  console.log(data)
 
   
   if(error){
@@ -21,19 +19,7 @@ export default function Home({date, data, setData, error, setError}: Props) {
   }
 
   return <div>
-    { data ? date.date ? <div className="container">
-        {data.media_type === "image" ?
-        <img className="universe-img" src={data.url} alt="..."/> : data.media_type === "video" ? <iframe className="youtube-video" src={data.url} title="Apod" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> :
-       <p className="no-img">Sorry, the media you are searching for is not found. Please pick another date to see the photo.</p>}
-        <div className="description">
-          <h2 className="img-title">{data.title}</h2>
-          <div className="date-container">
-            <p>Date: </p>
-            <p>{data.date}</p>
-          </div>
-          <p>{data.explanation}</p>
-        </div> 
-      </div> : (date.startDate && date.endDate) || date.count ? <div className="gallery">{data?.map((img: FetchedData) => img.media_type === "image" ? <img key={img.date} src={img.url} alt={img.title}/> : img.media_type === "video" ? <iframe key={img.date} src={img.url} id="youtube-video"title="Apod" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> :
+    { data ? date.date ? <Picture data={data}/> : (date.startDate && date.endDate) || date.count ? <div className="gallery">{data?.map((img: FetchedData) => img.media_type === "image" ? <img key={img.date} src={img.url} alt={img.title}/> : img.media_type === "video" ? <iframe key={img.date} src={img.url} id="youtube-video"title="Apod" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> :
   <p key={img.date} className="no-img">Sorry, the media you are searching for is not found. Please pick another date to see the photo.</p>)}</div> : <h2 className="loader">Loading...</h2>: <h2 className="loader">Loading...</h2>}
   </div>
 }
--- src/fetchData.ts
+++ src/fetchData.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import { Dispatch, SetStateAction } from "react";
 import { date, FetchedData } from "./types";
 
-export default function fetchData(date: date, setData:Dispatch<SetStateAction<FetchedData | FetchedData[] | any>>, setError: Dispatch<SetStateAction<FetchedData | FetchedData[] | any>>) {
-  const apiKey: String = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFXd";
-  let url=`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
-  let photoRequest = `&date=${date.date}`
-  let galleryStartDateRequest = `&start_date=${date.startDate}`
-  let galleryEndDateRequest = `&end_date=${date.endDate}`
-  let galleryCount = `&count=${date.count}`
+export default function fetchData(date: date, setData:Dispatch<SetStateAction<FetchedData | FetchedData[] | any>>, setError: Dispatch<SetStateAction<FetchedData | FetchedData[] | any>>): {} | void {
+  const apiKey: string = "NPzkN3QlcDnfIngrGKC7YuibZ7XmrSzPt2gQYFXd";
+  let url: string=`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+  let photoRequest: string = `&date=${date.date}`
+  let galleryStartDateRequest: string = `&start_date=${date.startDate}`
+  let galleryEndDateRequest:string = `&end_date=${date.endDate}`
+  let galleryCount:string  = `&count=${date.count}`
   let request: string = "";
   console.log("end date is: " + date.endDate)
   if(date.date) {
@@ -20,8 +20,6 @@ export default function fetchData(date: date, setData:Dispatch<SetStateAction<Fe
   if(date.count) {
       request = url + galleryCount
   }
-
-  console.log("request url is: " + request)
   
   if (request !== "") {
     axios.get(request).then(data => setData(data.data)).catch(error => setError(error.toJSON()));
